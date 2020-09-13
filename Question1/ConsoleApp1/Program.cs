b3f83f7bb317c7647b78543090c4f627985d207c
玉林 王 <746866547@qq.com>
2020-09-13 20:36:31 +0800

    submit 3

---
+++ Question1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Q1
+{
+    class PrimeNumber
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            string s = "";
+            s = Console.ReadLine();
+            a = Int32.Parse(s);
+
+            for (int i = 1; i <= a; i++)
+            {
+                int b = a % i;
+                if (b == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            s = Console.ReadLine();
+        }
+    }
+}
